286b3aa620d0fd60d297a2409671b28f1e40009a
Yuta Sugiyama <y.sugiyama54@gmail.com>
2021-05-11 23:03:21 +0900

    Step10 Implementing POST Method to create User Resource

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +23,10 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
 	}
 }
