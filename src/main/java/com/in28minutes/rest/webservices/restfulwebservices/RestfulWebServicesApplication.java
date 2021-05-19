12d5634373f5bc6b1cae46cfea62ffc0cdc22909
Yuta Sugiyama <y.sugiyama54@gmail.com>
2021-05-19 22:21:46 +0900

    Step18 Internationalization for RESTful Services2

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -19,7 +19,7 @@ public class RestfulWebServicesApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 
 		localeResolver.setDefaultLocale(Locale.US);
 
